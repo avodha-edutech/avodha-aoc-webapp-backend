77e45ff2742496d1fd78a8c77171cf2860ff3af3
avodha <115546855+avodha-edutech@users.noreply.github.com>
2022-12-21 22:15:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config({path: __dirname + '/.env'});
 const public = require('./routes/public');
 const routes = require('./routes/routes');
+const cors = require('cors');
 
 const port = process.env.SERVERPORT;
 
 const app = express();
 
+app.use(cors({
+cerdentials:true
+}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
